0214f1335f69beb397ccf59e0834fa9c1285763b
hounslow <hounslowmatthew@gmail.com>
2018-03-07 11:49:41 -0800

    Stream piping is working better now

--- new-attempt.js
+++ new-attempt.js
@@ -16,15 +16,14 @@ let statObject = {
 
 const objectTransform = new Transform({
   readableObjectMode: true,
-  // writableObjectMode: true,
   transform(chunk, encoding, error) {
     console.log('Reading %d characters of string data', chunk.length);
-    let currentTime = Date.now();
+    statObject.endTime = Date.now();
     statObject.totalLengthInBytes += chunk.length;
-    statObject.elapsed_time = (statObject.start_time - currentTime) / 1000;
+    statObject.elapsedTime += (statObject.endTime - statObject.startTime) / 1000;
     let dataString = chunk.toString();
     let split_lines = dataString.split("\n");
-    statObject.total_lines += split_lines.length - 1;
+    statObject.totalLines += split_lines.length - 1;
     this.push(statObject);
     error();
   },
@@ -34,7 +33,7 @@ const writer = new Writable({
   objectMode: true,
   write(chunk, encoding, callback) {
     statObject.throughput = statObject.totalLengthInBytes / statObject.elapsedTime;
-    console.log(chunk);
+    console.log(`Total read lines: ${statObject.totalLengthInBytes}, throughput: ${statObject.throughput}, total elapsed time: ${statObject.elapsedTime}`);
     callback();
   },
 })
