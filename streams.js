b8df4479cad1f10328cb81e40f93a2c8e9a488dd
hounslow <hounslowmatthew@gmail.com>
2018-03-09 15:11:12 -0800

    Trying to do a bit more cleanup with streams.js

--- streams.js
+++ streams.js
@@ -17,7 +17,6 @@ let destination = fs.createWriteStream(destinationPath, {flags: 'a'});
 // Object containing all of the information we care about
 let statObject = {
   "startTime": process.hrtime(),
-  "timeInSeconds": 0,
   "elapsedTime": 0,
   "totalBytes": 0,
   "currentByteCount": 0,
@@ -25,10 +24,14 @@ let statObject = {
   "throughput": 0,
 };
 
+source.on('error', (error) => {
+  console.log('An error occurred!');
+  console.log(error);
+})
 
 const objectTransform = new Transform({
   readableObjectMode: true,
-  transform(chunk, encoding, error) {
+  transform(chunk, encoding, callback) {
     let diff = process.hrtime(statObject.startTime);
     statObject.timeInMilliSeconds = (diff[0] *  1000) + (diff[1] / 1000000);
     statObject.currentByteCount = chunk.length;
@@ -37,27 +40,28 @@ const objectTransform = new Transform({
     let split_lines = chunk.toString().split("\n");
     statObject.totalLines += split_lines.length - 1;
     this.push(statObject);
-    error();
+    callback();
   },
 });
 
 const writer = new Writable({
   objectMode: true,
-  write(chunk, encoding, error) {
+  write(chunk, encoding, callback) {
     statObject.throughput = statObject.currentByteCount / statObject.timeInMilliSeconds;
     console.log(`total bytes read: ${statObject.totalBytes} bytes, total lines: ${statObject.totalLines}, current throughput: ${statObject.throughput} bytes/millisecond, total elapsed time: ${statObject.elapsedTime} milliseconds`);
-    error();
+    callback();
   },
 });
 
-writer.on('pipe', () => {
-  console.log(`Analysing file ${sourcePath}`);
-})
+writer.on('error', (error) => {
+  console.log("An error occurred while writing!");
+  console.log(error);
+});
 
 writer.on('finish', () => {
   destination.write(`filename: ${sourcePath}, total bytes read: ${statObject.totalBytes} bytes, total lines: ${statObject.totalLines}, throughput: ${statObject.throughput} bytes/millisecond, total elapsed time: ${statObject.elapsedTime} milliseconds\n`);
-  console.log('Finished.')
-})
+  console.log('Finished.');
+});
 
 source.pipe(objectTransform).pipe(writer);
 
